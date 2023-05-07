6a8a49dcf35ef99e10e9e9607ff37ad12dc9fd16
Jason Yip <bestdecoy@foxmail.com>
2023-05-07 17:32:05 +0800

    modify something wrong

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import shutil
 
 
 def sort_by_time(file_path):
+    # 按时间顺序排序文件
     files = os.listdir(file_path)
     if not files:
         return
@@ -13,6 +14,7 @@ def sort_by_time(file_path):
 
 
 def rm_not_gif(gif_all):
+    # 在列表内，移除不是gif的项
     del_not_gif = []
     for i in range(len(gif_all)):
         if gif_all[i][-4:] != ".gif":
@@ -20,11 +22,12 @@ def rm_not_gif(gif_all):
     for i in del_not_gif:
         gif_all.remove(i)
 
-
+# 路径
 wechat_file = r"C:\Users\bbsa2\Documents\WeChat Files"
 user_id = os.listdir(wechat_file)
 output_path = os.path.expanduser("~\\Desktop")  # 保存至桌面
 
+# 获取用户列表，删掉无关文件夹
 try:
     user_id.remove('All Users')
     user_id.remove('Applet')
@@ -36,7 +39,6 @@ except:
         print("WeChat File不完整。")
 
 # 拼接
-
 # 选择单用户
 if len(user_id) > 1:
     print("Wechat File包含的多用户列表如下：")
@@ -50,9 +52,11 @@ gif_dir = os.path.join(wechat_file, user_id[0], "FileStorage", "File",
                        time.strftime("%Y-%m", time.localtime()))  # gif文件夹地址
 gif_all = os.listdir(gif_dir)  # gif按时间顺序排序的列表
 rm_not_gif(gif_all)  # 移除非gif项
-for i in gif_all:
+
+for i in gif_all:  # 移除所有gif，防止干扰
     os.remove(os.path.join(gif_dir, i))
-f = open(os.path.join(gif_dir, "wxezGIF.gif"), "w")
+
+f = open(os.path.join(gif_dir, "wxezGIF.gif"), "w")  # 创建空gif，防止程序错误
 f.close()
 
 print("等待用户操作...")
@@ -72,9 +76,3 @@ while 1:
 
 shutil.copy(output_file, output_path)
 print("已复制到指定文件夹。")
-
-try:
-    for i in range(len(gif_all)):
-        os.remove(os.path.join(gif_dir, gif_all[i]))
-except:
-    pass
